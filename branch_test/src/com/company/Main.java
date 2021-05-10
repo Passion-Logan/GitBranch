05b81e05145954662e0f20eacf84c170901db3ad
Passion-Logan <wangdadamailbox@163.com>
2021-05-10 10:54:01 +0800

    first

--- branch_test/src/com/company/Main.java
+++ branch_test/src/com/company/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    private static void newBranch() {
+        System.out.println("这是新分支");
+    }
+
 }

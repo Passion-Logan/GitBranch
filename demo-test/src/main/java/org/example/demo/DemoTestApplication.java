624434b6c985db0b9f99f9a1e16c09d5998dd73e
Passion-Logan <wangdadamailbox@163.com>
2021-11-12 15:14:12 +0800

    update

---
+++ demo-test/src/main/java/org/example/demo/DemoTestApplication.java
@@ -0,0 +1,20 @@
+package org.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wql
+ * @desc DemoTestApplication
+ * @date 2021/11/12
+ * @lastUpdateUser wql
+ * @lastUpdateDesc
+ * @lastUpdateTime 2021/11/12
+ */
+@SpringBootApplication
+public class DemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestApplication.class, args);
+    }
+}
---
+++ demo-test/src/main/java/org/example/demo/controller/TestController.java
@@ -0,0 +1,23 @@
+package org.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wql
+ * @desc TestController
+ * @date 2021/11/12
+ * @lastUpdateUser wql
+ * @lastUpdateDesc
+ * @lastUpdateTime 2021/11/12
+ */
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("hello")
+    public String test() {
+        return "hello";
+    }
+}
